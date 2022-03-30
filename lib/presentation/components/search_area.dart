3c77f741c104ac30a39a3c71114e6ea6eaaf9e34
unknownMuse <elselwyn@gmail.com>
2022-03-30 16:44:24 +0100

    DONE

--- lib/presentation/components/search_area.dart
+++ lib/presentation/components/search_area.dart
@@ -4,30 +4,13 @@ import 'package:weather_app/utils/utils.dart';
 
 final cityProvider = StateProvider<String>((ref) => 'Lisbon');
 
-class CitySearchArea extends ConsumerStatefulWidget {
+class CitySearchArea extends ConsumerWidget {
   const CitySearchArea({Key? key}) : super(key: key);
 
-  @override
-  ConsumerState<ConsumerStatefulWidget> createState() => _CitySearchAreaState();
-}
-
-class _CitySearchAreaState extends ConsumerState<CitySearchArea> {
-  late final _searchController = TextEditingController();
+  static final _searchController = TextEditingController();
 
   @override
-  void initState() {
-    super.initState();
-    _searchController.text = ref.read(cityProvider);
-  }
-
-  @override
-  void dispose() {
-    _searchController.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Padding(
       padding: symmetricHorizontalPadding,
       child: Row(
@@ -36,6 +19,7 @@ class _CitySearchAreaState extends ConsumerState<CitySearchArea> {
           Expanded(
             child: SizedBox(
               child: TextField(
+                controller: _searchController,
                 textAlign: TextAlign.center,
                 style: Theme.of(context).textTheme.bodyText1,
                 cursorColor: AppColors.textColor,
@@ -87,7 +71,6 @@ class _CitySearchAreaState extends ConsumerState<CitySearchArea> {
             onTap: () {
               FocusScope.of(context).unfocus();
               ref.read(cityProvider.state).state = _searchController.text;
-              print(cityProvider);
             },
           ),
         ],
@@ -95,78 +78,3 @@ class _CitySearchAreaState extends ConsumerState<CitySearchArea> {
     );
   }
 }
-
-
-// class CitySearchArea extends ConsumerWidget {
-//   const CitySearchArea({Key? key}) : super(key: key);
-
-//   static final _searchController = TextEditingController();
-
-//   @override
-//   Widget build(BuildContext context, WidgetRef ref) {
-//     return Padding(
-//       padding: symmetricHorizontalPadding,
-//       child: Row(
-//         mainAxisAlignment: MainAxisAlignment.center,
-//         children: [
-//           Expanded(
-//             child: SizedBox(
-//               child: TextField(
-//                 textAlign: TextAlign.center,
-//                 style: Theme.of(context).textTheme.bodyText1,
-//                 cursorColor: AppColors.textColor,
-//                 decoration: const InputDecoration(
-//                   fillColor: AppColors.searchFillColor,
-//                   filled: true,
-//                   focusedBorder: OutlineInputBorder(
-//                     borderSide: BorderSide(
-//                       color: AppColors.textColor,
-//                       width: 2,
-//                     ),
-//                     borderRadius: BorderRadius.only(
-//                       topLeft: Radius.circular(10),
-//                       bottomLeft: Radius.circular(10),
-//                     ),
-//                   ),
-//                   border: OutlineInputBorder(
-//                     borderSide: BorderSide(color: AppColors.textColor),
-//                     borderRadius: BorderRadius.only(
-//                       topLeft: Radius.circular(10),
-//                       bottomLeft: Radius.circular(10),
-//                     ),
-//                   ),
-//                 ),
-//                 onSubmitted: (value) =>
-//                     ref.read(cityProvider.state).state = value,
-//               ),
-//             ),
-//           ),
-//           InkWell(
-//             child: Container(
-//               height: SizeConfig.blockSizeVertical * 6.5,
-//               alignment: Alignment.center,
-//               decoration: const BoxDecoration(
-//                 color: AppColors.utilityColor,
-//                 borderRadius: BorderRadius.only(
-//                   topRight: Radius.circular(10),
-//                   bottomRight: Radius.circular(10),
-//                 ),
-//               ),
-//               child: Padding(
-//                 padding: const EdgeInsets.symmetric(horizontal: 15),
-//                 child: Text(
-//                   Strings.search,
-//                   style: Theme.of(context).textTheme.headline3,
-//                 ),
-//               ),
-//             ),
-//             onTap: () {
-//               FocusScope.of(context).unfocus();
-//               ref.read(cityProvider.state).state = _searchController.text;
-//             },
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
